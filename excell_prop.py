2d1f00655ea9efa498c8689e6e73d2ffbb487ae3
S_Motlaq <motlaq@aut.ac.ir>
2021-07-28 02:45:29 +0430

    added excell manager

---
+++ excell_prop.py
@@ -0,0 +1,7 @@
+header = 'گزارش ریز ساعات کاری % ماه'
+titles = ['روز','شروع','پایان','مجموع']
+sum_all = 'مجموع ماه'
+
+sum_all_format = {'bold': True, 'valign':'vcenter', 'align':'center'}
+sum_of_day_format = {'num_format': '#.##', 'valign':'vcenter', 'align':'center'}
+regular_format = {'valign':'vcenter', 'align':'center'}
